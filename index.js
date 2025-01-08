046472045f34be843b256f5bd6edeab7741e0911
jassi5657 <preetsansoya008@gmail.com>
2025-01-08 10:09:05 +0530

    get added

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const _ = require('lodash');
 
 app.use(cors());
 
+app.get("/",(req,res)=>{
+  res.send("server is running")
+})
+
 // Set up multer for file uploads
 const upload = multer({ dest: 'uploads/' });
 
